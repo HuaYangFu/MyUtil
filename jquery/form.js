b5fea2d4cbdbfc19a0c4352049ccdaa9136a572c
華暘 Jacky <ms2023645@gmail.com>
2018-01-07 23:53:29 +0800

    resetform

---
+++ jquery/form.js
@@ -0,0 +1,6 @@
+function resetForm($form) {
+   event.preventDefault();
+   $(':input',$form).not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');
+   $form.find('input:text, input:password, input:file, select, textarea').val('');
+   $form.find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
+}
